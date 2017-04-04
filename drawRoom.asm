28e4d19359653d87938640d4150c5c4049264aed
tEFFx <erikkoii@gmail.com>
2017-04-04 21:50:30 +0200

    Room draw routine to save memory (current room only uses 55bytes!)

--- drawRoom.asm
+++ drawRoom.asm
@@ -1,3 +1,80 @@
+currX           BYTE $00
+currY           BYTE $00
+currChar        BYTE $00
+currLen         BYTE $00
+currCol         BYTE $00
+
+defm setPointers
+                lda rowTable,y          ;store pointer to row
+                sta zeroPtr
+                lda rowTable+1,y
+                sta zeroPtr+1
+                lda colorRamTable,y
+                sta zeroPtr1
+                lda colorRamTable+1,y
+                sta zeroPtr1+1
+                endm
+
+drawRoom        ldx #$00
+@goNext         lda $4000,x
+                cmp #$ff
+                beq @return
+                sta currY
+                lda $4001,x
+                sta currX
+                lda $4002,x             ;store char
+                sta currChar
+                lda $4004,x
+                and #$0f
+                sta currCol
+                lda $4004,x
+                and #$80
+                cmp #$80                ;is vertical?
+                beq @vertical
+                jmp @horizontal
+@return         rts
+@horizontal     dec currX
+                lda currY
+                asl
+                tay
+                setPointers
+                lda $4003,x             ;store len
+                adc currX               ;set counter
+                tay
+                iny
+@horiLoop       cpy currX
+                beq @continue
+                lda currChar
+                sta (zeroPtr),y
+                lda currCol
+                sta (zeroPtr1),y
+                dey
+                jmp @horiLoop
+@vertical       lda $4003,x
+                adc currY
+                sta currLen
+                dec currY
+@vertLoop       lda currLen
+                cmp currY
+                beq @continue
+                asl
+                tay
+                setPointers
+                ldy currX
+                lda currChar
+                sta (zeroPtr),y
+                lda currCol
+                sta (zeroPtr1),y
+                dec currLen
+                jmp @vertLoop
+@continue       inx
+                inx
+                inx
+                inx
+                inx
+                jmp @goNext                
+                rts
+
 defm            drawLine                ;/1 = lineoffset
                 ldx #$00
 @loop           lda $4000+/1,x
@@ -10,7 +87,7 @@ defm            drawLine                ;/1 = lineoffset
                 bne @loop
                 endm
 
-drawRoom        drawLine 0
+drawBg          drawLine 0
                 drawLine 200
                 drawLine 400
                 drawLine 600
--- main.asm
+++ main.asm
@@ -1,6 +1,14 @@
+; 10 SYS (4096)
+
+*=$0801
+
+        BYTE    $0E, $08, $0A, $00, $9E, $20, $28,  $34, $30, $39, $36, $29, $00, $00, $00
 
 *=$1000
-init            jsr drawRoom
+init            jsr clearScr
+                lda #$0b
+                sta $d021
+                jsr drawRoom
                 lda #$02                ;set up sprite multicolor
                 sta $d025
                 lda #$05
@@ -29,6 +37,21 @@ init            jsr drawRoom
 irq             dec $d019
                 jsr updatePlayer
                 jmp $ea81 
+
+clearScr        lda #$20
+                ldx #$00
+@loop           sta $0400,x
+                sta $0500,x
+                sta $0600,x
+                sta $06e8,x
+                inx
+                bne @loop
+                rts
+
+rowTable        BYTE $0400,$0428,$0450,$0478,$04A0,$04C8,$04F0,$0518,$0540,$0568,$0590,$05B8,$05E0,$0608,$0630,$0658,$0680,$06A8,$06D0,$06F8,$0720,$0748,$0770,$0798,$07C0
+colorRamTable   BYTE $D800,$D828,$D850,$D878,$D8A0,$D8C8,$D8F0,$D918,$D940,$D968,$D990,$D9B8,$D9E0,$DA08,$DA30,$DA58,$DA80,$DAA8,$DAD0,$DAF8,$DB20,$DB48,$DB70,$DB98,$DBC0
+zeroPtr = $FB
+zeroPtr1 = $FE
 
 Incasm "drawRoom.asm"
 Incasm "joystick.asm"
