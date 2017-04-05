9885196402939c05b0892ec8eb140e9a7b0567ac
tEFFx <erikkoii@gmail.com>
2017-04-05 21:22:04 +0200

    Improved room drawing routine

--- drawRoom.asm
+++ drawRoom.asm
@@ -1,8 +1,10 @@
-currX           BYTE $00
-currY           BYTE $00
-currChar        BYTE $00
-currLen         BYTE $00
-currCol         BYTE $00
+xCurr           BYTE $00
+yCurr           BYTE $00
+xEnd            BYTE $00
+yEnd            BYTE $00
+char            BYTE $00
+color           BYTE $00
+currOffset      BYTE $00
 
 defm setPointers
                 lda rowTable,y          ;store pointer to row
@@ -15,81 +17,45 @@ defm setPointers
                 sta zeroPtr1+1
                 endm
 
-drawRoom        ldx #$00
-@goNext         lda $4000,x
-                cmp #$ff
-                beq @return
-                sta currY
-                lda $4001,x
-                sta currX
-                lda $4002,x             ;store char
-                sta currChar
-                lda $4004,x
-                and #$0f
-                sta currCol
-                lda $4004,x
-                and #$80
-                cmp #$80                ;is vertical?
-                beq @vertical
-                jmp @horizontal
-@return         rts
-@horizontal     dec currX
-                lda currY
-                asl
-                tay
-                setPointers
-                lda $4003,x             ;store len
-                adc currX               ;set counter
-                tay
-                iny
-@horiLoop       cpy currX
-                beq @continue
-                lda currChar
-                sta (zeroPtr),y
-                lda currCol
-                sta (zeroPtr1),y
-                dey
-                jmp @horiLoop
-@vertical       lda $4003,x
-                adc currY
-                sta currLen
-                dec currY
-@vertLoop       lda currLen
-                cmp currY
-                beq @continue
-                asl
-                tay
-                setPointers
-                ldy currX
-                lda currChar
-                sta (zeroPtr),y
-                lda currCol
-                sta (zeroPtr1),y
-                dec currLen
-                jmp @vertLoop
-@continue       inx
-                inx
-                inx
-                inx
-                inx
-                jmp @goNext                
-                rts
-
-defm            drawLine                ;/1 = lineoffset
-                ldx #$00
-@loop           lda $4000+/1,x
-                sta $0400+/1,x
-                lda $43E8+/1,x
-                sta $D800+/1,x
-                inx
-                clc
-                cpx #$C8
-                bne @loop
+defm strRoomData
+                lda room0+/1,x
+                sta /2
                 endm
 
-drawBg          drawLine 0
-                drawLine 200
-                drawLine 400
-                drawLine 600
-                drawLine 800
-                rts
+drawRoom        lda #$00
+                sta currOffset
+@loop           ldx currOffset
+                cpx room0               ;first byte is length
+                beq @end
+                strRoomData 1, xCurr
+                strRoomData 2, xEnd
+                inc xEnd
+                strRoomData 6, char
+                strRoomData 5, color
+                strRoomData 4, yEnd
+                inc yEnd
+                strRoomData 3, yCurr
+                tax
+@rowLoop        cpx yEnd             ;compare with end y
+                beq @incOffset          ;go to next shape if over
+                txa
+                asl
+                tay
+                setPointers
+                ldy xCurr
+@colLoop        cpy xEnd             ;compare with end x
+                beq @incRow
+                lda char
+                sta (zeroPtr),y
+                lda color
+                sta (zeroPtr1),y
+                iny
+                jmp @colLoop
+@incRow         inx
+                jmp @rowLoop
+@incOffset      lda currOffset
+                clc
+                adc #$06
+                sta currOffset
+                jmp @loop
+@end            rts
